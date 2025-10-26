801b2822021b22e37c0487eb6e50f98268d64a6f
Am1re121 <usmonovamirjon2@gmail.com>
2025-10-26 12:02:39 +0500

    dec practis

--- Practice/decorator.js
+++ Practice/decorator.js
@@ -1,3 +1,5 @@
+const { text } = require("express")
+
 function sum() {
     let i=0
     let counter=0
@@ -10,13 +12,30 @@ function sum() {
 
 function decoratorsun(fn) {
     return function(...args) {
-        const odd = args.filter(num => num % 2 !== 0);
-        return fn(...odd);
+        const nechet = args.filter(num => num % 2 !== 0);
+        return fn(...nechet);
             
-        
     }
 }
 
 let decsun = decoratorsun(sum)
 
 console.log(decsun(1,2,3,4,5))
+
+
+function DecoratorPractice(fn) {
+    return function(txt) {
+        console.log("decorator cool")
+        return fn(txt)
+    }
+}
+
+
+function decprac() {
+    console.log("Decorator Practice")
+}
+
+let result = DecoratorPractice(decprac)
+
+result()
+
