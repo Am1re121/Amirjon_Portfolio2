1ad127003025e7eadbdb133cc6a9f8c7f067207f
Am1re121 <usmonovamirjon2@gmail.com>
2026-05-03 08:51:25 +0500

    add lesson dart

---
+++ Dart/lesson.dart
@@ -0,0 +1,21 @@
+void main() {
+    String name = "Dart";
+    String? surname;
+    print("Hello, $name!");
+    print("Surname: $surname");
+
+    var city = "New York";
+    city = "Los Angeles";
+    print("I live in $city.");
+
+    int? age;
+    print("Age: $age");
+
+    int? num;
+    print("Number: $num");
+
+    dynamic data = "Hello";
+    print("Data: $data");
+    data = 123;
+    print("Data: $data"); 
+}
